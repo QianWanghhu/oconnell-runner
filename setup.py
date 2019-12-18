789c72f463f4bb949d1d99bc0a5c0ac9592f73e3
Qian Wang <qian@hhu.edu.cn>
2019-12-18 14:25:04 +1100

    Initial file commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Source Runner',
+    version='0.1dev',
+    packages=['source_runner',],
+    license='MIT license Copyright 2019',
+    long_description='',
+)
